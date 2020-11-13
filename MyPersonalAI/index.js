de9f52c3b5dc3053265bed1a058cdfd111fcdff2
elRaulito <42917076+elRaulito@users.noreply.github.com>
2020-11-13 16:43:36 +0100

    Add files via upload

---
+++ MyPersonalAI/index.js
@@ -0,0 +1,80 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var ba64 = require("ba64");
+const tf = require('@tensorflow/tfjs')
+const mobilenet = require('@tensorflow-models/mobilenet');
+const cocoSsd = require('@tensorflow-models/coco-ssd');
+global.fetch = require('node-fetch');
+require('@tensorflow/tfjs-node')
+
+var jsonParser=bodyParser.json()
+
+const fs = require('fs');
+const jpeg = require('jpeg-js');
+
+const NUMBER_OF_CHANNELS = 3
+
+const readImage = path => {
+  const buf = fs.readFileSync(path)
+  const pixels = jpeg.decode(buf, true)
+  return pixels
+}
+
+const imageByteArray = (image, numChannels) => {
+  const pixels = image.data
+  const numPixels = image.width * image.height;
+  const values = new Int32Array(numPixels * numChannels);
+
+  for (let i = 0; i < numPixels; i++) {
+    for (let channel = 0; channel < numChannels; ++channel) {
+      values[i * numChannels + channel] = pixels[i * 4 + channel];
+    }
+  }
+
+  return values
+}
+
+const imageToInput = (image, numChannels) => {
+  const values = imageByteArray(image, numChannels)
+  const outShape = [image.height, image.width, numChannels];
+  const input = tf.tensor3d(values, outShape, 'int32');
+
+  return input
+}
+
+const loadModel = async path => {
+  const mn = new mobilenet.MobileNet(1, 1);
+  await mn.load()
+  return mn
+}
+
+
+
+app.get("/url",jsonParser, (req, res, next) => {
+ var contenuto=req.body;   
+ console.log(contenuto);
+ res.json(contenuto);
+});
+
+app.post("/image",jsonParser,async (req,res,next)=>{
+  var image=req.body.immagine; 
+  image="data:image/jpeg;base64,"+image;
+
+  ba64.writeImageSync("myimage", image);
+
+  const value = readImage("myimage.jpeg")
+  const input = imageToInput(value, NUMBER_OF_CHANNELS)
+
+  const model= await cocoSsd.load()
+  const predictions = await model.detect(input)
+
+  //console.log('classification results:', predictions)
+  
+    res.json(predictions);
+});
+         
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
